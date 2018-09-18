b69d325396193e2e5fdfb6ee01d4841cb720ee2c
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-09-18 17:01:26 +0100

    Initial commit

---
+++ ChangeCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
