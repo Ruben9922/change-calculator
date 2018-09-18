4bce8ed2ceceecb5b0642a828357c916f1cea808
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-09-18 21:25:26 +0100

    Display remainder

--- ChangeCalculator/Program.cs
+++ ChangeCalculator/Program.cs
@@ -16,28 +16,31 @@ namespace ChangeCalculator
             decimal amount = ConsoleUtilities.ReadDecimal("Enter amount: ", 0);
             List<decimal> coins = new List<decimal> {2, 1, 0.5m, 0.2m, 0.1m, 0.05m, 0.02m, 0.01m};
 
-            List<int> coinQtys = ComputeChange(amount, coins);
+            List<int> coinQtys = ComputeChange(amount, coins, out decimal remainder);
 
             Console.WriteLine();
             Console.Write(coins.Zip(coinQtys, (coin, coinQty) => coin + ": " + coinQty + "\n").Aggregate(new StringBuilder(), (sb, s) => sb.Append(s)));
+            Console.WriteLine();
+            Console.WriteLine("Remainder: " + remainder);
         }
 
-        private static List<int> ComputeChange(decimal amount, List<decimal> coins)
+        private static List<int> ComputeChange(decimal amount, List<decimal> coins, out decimal remainder)
         {
-            decimal remainder = amount;
+            decimal currentRemainder = amount;
             decimal smallestCoin = coins.Min();
             List<int> coinQtys = Enumerable.Repeat(0, coins.Count).ToList();
-            while (remainder >= smallestCoin)
+            while (currentRemainder >= smallestCoin)
             {
-                List<decimal> allowedCoins = coins.FindAll(coin => coin <= remainder);
+                List<decimal> allowedCoins = coins.FindAll(coin => coin <= currentRemainder);
                 decimal largestAllowedCoin = allowedCoins.Max();
                 int largestAllowedCoinIndex = coins.IndexOf(largestAllowedCoin);
                 
                 coinQtys[largestAllowedCoinIndex]++; // TODO: Possibly replace with objects
 
-                remainder -= largestAllowedCoin;
+                currentRemainder -= largestAllowedCoin;
             }
 
+            remainder = currentRemainder;
             return coinQtys;
         }
     }
